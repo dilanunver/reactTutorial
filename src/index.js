485fd31390ea95cd3b3df095443cbfc57350ea6b
dilanunver <dunver33@gmail.com>
2021-03-15 16:33:15 +0300

    starting React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+}
+const Image = () => (
+  <img src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg"
+    alt=""
+  />
+)
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>
+const Author = () => <h4>
+  Amelia Hepworth</h4>
+
+// JSX Rules
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
+// function Greeting() {
+//   return (
+
+//     <div>
+//       <h3>Hello people</h3>
+//       <ul>
+//         <li>
+//           <a href="#">hello world</a>
+//         </li>
+//         <img src='' alt=''></img>
+//       </ul>
+//     </div>
+
+//   );
+// }
+
+// const Greeting = () => {
+//   return React.createElement('div', {}, React.createElement('h1', {}, 'hello world'));
+// }
+
+
+// Nested Components, React Tools
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <Person />
+//       <Name />
+//     </div>
+//   )
+// }
+// const Person = () => <h1>Hello world</h1>;
+// const Name = () => {
+//   return <p> John Doe</p>
+// }
+
+
+ReactDom.render(<BookList />, document.getElementById('root'))
